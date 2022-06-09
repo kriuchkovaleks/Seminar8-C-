5c4528ab8cb739e46e977bf5ea214291e054f343
kriuchkovaleks <103981431+kriuchkovaleks@users.noreply.github.com>
2022-06-09 10:46:50 +0300

    Add files via upload

---
+++ Task58.cs
@@ -0,0 +1,84 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+int GetDemension(string message)
+{
+    Console.WriteLine(message);
+    int demension = int.Parse(Console.ReadLine());
+    return demension;
+}
+
+int[,] InitMatrix(int firstDemension, int secondDemension)
+{
+    int[,] matrix = new int[firstDemension, secondDemension];
+    Random rnd = new Random();
+    for (int i = 0; i < firstDemension; i++)
+    {
+        for (int j = 0; j < secondDemension; j++)
+            matrix[i, j] = rnd.Next(0, 10);
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} ");
+
+        Console.WriteLine();
+    }
+}
+
+void GetMultiplyMatrix(int[,] firstMatrix, int[,] secondMatrix)
+{
+    
+    if (firstMatrix.GetLength(0) != secondMatrix.GetLength(1))
+        Console.WriteLine("Данные матрицы нельзя перемножить");
+    else
+    {
+        int [,] resultMatrix = new int[firstMatrix.GetLength(0), secondMatrix.GetLength(1)];
+        
+        for (int i = 0; i < firstMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < secondMatrix.GetLength(1); j++)
+            {
+                for (int k = 0; k < secondMatrix.GetLength(1); k++)
+                {
+                    resultMatrix[i,j] = resultMatrix[i,j] + firstMatrix[i,k] * secondMatrix[k,j];
+                   
+                }
+            }
+            
+        }
+        Console.WriteLine("Переумноженная матрица");
+        PrintMatrix(resultMatrix);
+
+    }    
+
+    
+}
+
+
+
+int firstDemensionMatrix1 = GetDemension("Введите длину для первого разряда матрицы:");
+int secondDemensionMatrix1 = GetDemension("Введите длину для второго разряда матрицы:");
+
+int[,] firstMatrix = InitMatrix(firstDemensionMatrix1, secondDemensionMatrix1);
+
+Console.WriteLine("Первая матрица:");
+PrintMatrix(matrix: firstMatrix);
+
+int firstDemensionMatrix2 = GetDemension("Введите длину для первого разряда матрицы:");
+int secondDemensionMatrix2 = GetDemension("Введите длину для второго разряда матрицы:");
+
+int[,] secondMatrix = InitMatrix(firstDemensionMatrix2, secondDemensionMatrix2);
+
+Console.WriteLine("Вторая матрица:");
+PrintMatrix(matrix: secondMatrix);
+
+GetMultiplyMatrix(firstMatrix, secondMatrix);
+
+
---
+++ Task60.cs
@@ -0,0 +1,54 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+
+int GetDemension(string message)
+{
+    Console.WriteLine(message);
+    int demension = int.Parse(Console.ReadLine());
+    return demension;
+}
+
+int[,,] InitMatrix(int firstDemension, int secondDemension, int thirdDemension)
+{
+    int[,,] matrix = new int[firstDemension, secondDemension, thirdDemension];
+    Random rnd = new Random();
+    for (int i = 0; i < firstDemension; i++)
+    {
+        for (int j = 0; j < secondDemension; j++)
+        {
+            for (int k = 0; k < thirdDemension; k++)
+            {
+                matrix[i, j, k] = rnd.Next(0, 10);
+            }
+        }
+            
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+      for (int j = 0; j < matrix.GetLength(1); j++)
+      {
+          for (int k = 0; k < matrix.GetLength(2); k++)
+           { Console.Write($"{matrix[i,j, k]} индекс {i}.{j}.{k}. ");
+
+             Console.WriteLine();
+           }
+      }
+        
+    }
+}
+
+
+int firstDemension = GetDemension("Введите длину для первого разряда матрицы:");
+int secondDemension = GetDemension("Введите длину для второго разряда матрицы:");
+int thirdDemension = GetDemension("Введите длину для третьего разряда матрицы:");
+
+int[,,] resultMatrix = InitMatrix(firstDemension, secondDemension, thirdDemension);
+
+PrintMatrix(matrix: resultMatrix);
